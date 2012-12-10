f8ba12c33712ffa23e2a206242f1bdea0a46cb61
Vanilla Thunder <hello@marat-bedoev.net>
2012-12-10 21:45:05 +0100

    initial commit

---
+++ copy_this/modules/vt-nca/metadata.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * vt Newest Category Articles
+ * Copyright (C) 2012  Marat Bedoev
+ * 
+ * This program is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>
+ */
+$aModule = array(
+    'id' => 'vt-nca',
+    'title' => '<strong style="color:#c700bb;border: 1px solid #c700bb;padding: 0 2px;background:white;">VT</strong> Newest Category Articles',
+    'description' => 'getting list of newest articles from a specal category<hr/>
+			<h2>usage:</h2>put this code into list.tpl to insert the list of newest articles
+			<textarea cols="80" rows="2">[{block name="category_newestarticles"}][{/block}]</textarea><br/>
+edit the vt-nca/out/blocks/category_newestarticles.tpl file to change the appearance',
+    'thumbnail' => 'oxid-vt.jpg',
+    'version' => '1.0',
+    'author' => 'Marat Bedoev',
+    'email' => 'oxid@marat-bedoev.net',
+    'url' => 'https://github.com/vanilla-thunder/',
+    'extend' => array(
+        'alist' => 'vt-nca/alist_nca',
+        'oxarticlelist' => 'vt-nca/oxarticlelist_nca',
+    ),
+    'blocks' => array(
+        array('template' => 'page/list/list.tpl', 'block' => 'category_newestarticles', 'file' => 'category_newestarticles.tpl')
+    )
+);
---
+++ copy_this/modules/vt-nca/oxarticlelist_nca.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * vt Newest Category Articles
+ * Copyright (C) 2012  Marat Bedoev
+ * 
+ * This program is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation;
+ * either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>
+ */
+class oxarticlelist_nca extends oxarticlelist_nca_parent {
+
+	public function loadNewestCategoryArticles($soxId = null) {
+
+		if ($soxId == null) {
+			return false;
+		}
+
+		$myConfig = $this->getConfig();
+
+		if (!$myConfig->getConfigParam('bl_perfLoadPriceForAddList')) {
+			$this->getBaseObject()->disablePriceLoad();
+		}
+
+		$this->_aArray = array();
+
+		$sArticleTable = getViewName('oxarticles');
+		if ($myConfig->getConfigParam('blNewArtByInsert')) {
+			$sType = 'oxinsert';
+		} else {
+			$sType = 'oxtimestamp';
+		}
+		$sSelect = "select * from $sArticleTable ";
+		$sSelect .= "RIGHT JOIN oxobject2category ON $sArticleTable.oxid = oxobject2category.oxobjectid ";
+		$sSelect .= "WHERE oxobject2category.oxcatnid = '" . $soxId . "' and " . $this->getBaseObject()->getSqlActiveSnippet() . " and oxissearch = 1 order by $sType desc ";
+
+		if (!($iLimit = (int) $iLimit)) {
+			$iLimit = $myConfig->getConfigParam('iNrofNewcomerArticles');
+		}
+		$sSelect .= "limit " . $iLimit;
+
+		$this->selectString($sSelect);
+	}
+
+}
